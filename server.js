8c5371005706bf12964511d99d3732a061019069
TWatkings <106554653+TWatkings@users.noreply.github.com>
2023-12-27 18:15:14 -0500

    create server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'index.html')));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
