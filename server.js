e2635c60aee5d475e9eb8287fcbbf036f0e590fa
TWatkings <106554653+TWatkings@users.noreply.github.com>
2024-01-16 11:18:39 -0500

    created project folder

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 
 // Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'index.html')));
+app.use(express.static(path.join(__dirname, 'project')));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
